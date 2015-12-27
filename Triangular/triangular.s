a1f9e2d93176099796a5afde0298d6ecb6b50908
Austin Herring <awh44@drexel.edu>
2015-12-27 01:42:00 -0500

    Add a 'real' int_print function
    The function prints the integer in the correct order. However, it doesn't correctly preserve registers (namely, things like %r12 - %r15), so it's not strictly 'correct' yet.

--- Triangular/triangular.s
+++ Triangular/triangular.s
@@ -5,7 +5,7 @@
 
 _exit:
 	movq $60, %rax #Exit is syscall 60
-	movq $123, %rdi #Exit with status 0
+	movq $0, %rdi #Exit with status 0
 	syscall #Execute the syscall
 
 /**
@@ -57,40 +57,49 @@ _atoi:
 		ret
 
 /**
-	_reverse_int_print: Prints the (unsigned) integer contained in %rdi in reverse order
+	_int_print: Prints the (unsigned) integer contained in %rdi
 		void int_print(int n);
 	@param %rdi - the integer to print
 */
-_reverse_int_print:
-	subq $4, %rsp #Make room on the stack for a single character
-	movq %rsp, %rsi #Pre set up the pointer to the character for the write syscall
-	movq $10, %r12 #Set up a constant for use with division
-	movq %rdi, %r13 #Move the argument to a temporary register
+_int_print:
+	subq $4, %rsp #Make room on the stack, but keep it 4-byte aligned
+	movb $10, 3(%rsp) #Add a newline to the end of the string
+	movq $3, %r12 #Indicate that there are three places left in the stack
+	movq $1, %r13 #Indicate that the string is of length 1
 
-	_print_loop_start:
-		movq %r13, %rax #Move the integer into the lower part of the division register
-		cqo #Sign extend %rax into %rdx
-		div %r12 #Perform the division by ten
-		movq %rax, %r13 #Move the quotient back to the temporary register for use next iteration
-		addq $48, %rdx #'Convert' the remainder to a character
-		movb %dl, (%rsp) #Move the character to the stack
+	movq $10, %r14 #Set up the 10 to be divided by
+	movq %rdi, %r15 #Move the argument to a temporary register
 
-		movq $1, %rax #1 is the syscall number for write
-		movq $1, %rdi #1 is the file descriptor for stdout
-		movq $1, %rdx #Printing only a single character
-		syscall
+	_int_print_loop_start:
+		movq %r15, %rax	#Set up the integer for division
+		cqo #Sign extend %rax into %rdx for division
+		div %r14 #Perform the division by 10
+		movq %rax, %r15 #Move the quotient back to the temporary register for the next iteration
+		addq $48, %rdx #'Convert' the integer remainder to a character
 
-		cmpq $0, %r13
-		jne _print_loop_start
+		cmpq $0, %r12 #See if the remaining room on the stack is zero
+		jne _int_print_room_remaining #If the remaining room is not zero, continue on
+		subq $4, %rsp #Otherwise, make room on the stack
+		movq $4, %r12 #Indicate that there are 4 bytes available on the stack
 
-	movb $10, (%rsi)
-	movq $1, %rax
-	movq $1, %rdi
-	movq %rsp, %rsi
-	movq $1, %rdx
-	syscall
+	_int_print_room_remaining:
+		subq $1, %r12 #Subtract one from the available room on the stack
+		movb %dl, (%rsp, %r12, 1) #Move the character to the stack at %rsp + %r10 * 1
+		addq $1, %r13 #Increase the length of the string by one
 
-	addq $4, %rsp #Restore the stack
+		cmpq $0, %r15
+		jne _int_print_loop_start
+
+	movq $1, %rax #1 is the syscall number for write
+	movq $1, %rdi #1 is the file descriptor for stdout
+	movq %rsp, %rsi #Make the stack pointer as buffer argument
+	addq %r12, %rsi #Add the remaining 'empty' room on the stack to the stack pointer value
+	movq %r13, %rdx #Indicate the length of the string/buffer to print
+	syscall #Execute the write syscall
+
+	addq %r13, %rsp #Add the string length back to the stack
+	add %r12, %rsp #Add the remaining stack space back to the stack
+
 	ret
 
 /**
@@ -167,7 +176,7 @@ _start:
 	#Set up the argument and call int_print
 	####################
 	movq %rax, %rdi
-	call _reverse_int_print
+	call _int_print
 	####################
 
 	call _exit
